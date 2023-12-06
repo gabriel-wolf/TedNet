9692b6fe115280cc153be9ea23bd4a0ab4cd6341
Gabriel Wolf <gabeericwolf@gmail.com>
2023-12-06 14:52:54 -0500

    more

--- js/centerpopup.js
+++ js/centerpopup.js
@@ -20,3 +20,35 @@ var showPopupButton = document.getElementById("show-popup-button");
 if (showPopupButton) {
     showPopupButton.addEventListener("click", openPopup);
 }
+
+  function showLarge1() {
+    // Display the overlay
+    document.getElementById("overlay1").style.display = "flex";
+
+    // Hide the overlay after 3 seconds (adjust as needed)
+    setTimeout(function() {
+      document.getElementById("overlay1").style.display = "none";
+    }, 3000);
+  }
+
+
+  function showLarge2() {
+    // Display the overlay
+    document.getElementById("overlay2").style.display = "flex";
+
+    // Hide the overlay after 3 seconds (adjust as needed)
+    setTimeout(function() {
+      document.getElementById("overlay2").style.display = "none";
+    }, 3000);
+  }
+
+
+  function showLarge3() {
+    // Display the overlay
+    document.getElementById("overlay3").style.display = "flex";
+
+    // Hide the overlay after 3 seconds (adjust as needed)
+    setTimeout(function() {
+      document.getElementById("overlay3").style.display = "none";
+    }, 3000);
+  }
